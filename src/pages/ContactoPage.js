d0856774221ee274908e7d1230f106221a0ad14c
Lorena Bedrune <lorenabedrune@gmail.com>
2022-08-28 01:22:34 -0300

    28/8

--- src/pages/ContactoPage.js
+++ src/pages/ContactoPage.js
@@ -1,44 +1,82 @@
-import './../styles/ContactoPage.css'
+import './../styles/ContactoPage.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 const ContactoPage = (props) => {
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+    //
+
     return (
-            <main className="holder contacto">
-                <div>
-                    <h2>Contacto Rápido</h2>
-                    <form action=" " method="" class="formulario">
-                        <p>
-                            <label for="nombre">Nombre</label>
-                            <input type="text" name="" />
-                        </p>
-                        <p>
-                            <label for="email">Email</label>
-                            <input type="text" name="" />
-                        </p>
-                        <p>
-                            <label for="telefono">Telefono</label>
-                            <input type="text" name="" />
-                        </p>
-                        <p>
-                            <label for="mensaje">Mensaje</label>
-                            <textarea name=""></textarea>
-                        </p>
-                        <p>
-                            <input type="submit" value="Enviar" />
-                        </p>
-                    </form>
-                </div>
-                <div className="datos">
-                    <h2>Redes</h2>
-                    <li>Email: contacto@trekkingar.com</li>
-                    <li>Facebook: trekkingar</li>
-                    <li>Twitter: trekkingar</li>
-                    <li>Instagram: trekkingar</li>
+        <main className="holder contacto">
+            <div>
+                <h2>Contacto Rápido</h2>
+                <form action="/contacto" method="post" class="formulario">
+                    <p>
+                        <label for="nombre">Nombre</label>
+                        <input type="text" name="nombre" value={formData.nombre} unChange={handleChange} />
+                    </p>
+                    <p>
+                        <label for="email">Email</label>
+                        <input type="text" name="email" value={formData.email} unChange={handleChange} />
+                    </p>
+                    <p>
+                        <label for="telefono">Telefono</label>
+                        <input type="text" name="telefono" value={formData.telefono} unChange={handleChange} />
+                    </p>
+                    <p>
+                        <label for="mensaje">Mensaje</label>
+                        <textarea name="mensaje" value={formData.mensaje} unChange={handleChange} ></textarea>
+                    </p>
+                    <p>
+                        <input type="submit" value="Enviar" />
+                    </p>
+                </form>
+
+                {sending ? <p>Enviando..</p> : null}
+                {msg ? <p>{msg}</p> : null}
+
+            </div>
+            <div className="datos">
+                <h2>Redes</h2>
+                <li>Email: contacto@trekkingar.com</li>
+                <li>Facebook: trekkingar</li>
+                <li>Twitter: trekkingar</li>
+                <li>Instagram: trekkingar</li>
 
 
 
-                </div>
-            </main>
-        
+            </div>
+        </main>
+
 
     );
 }
